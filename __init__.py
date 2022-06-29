d79ac8fc315317e28da130f99ec3b98b3a517849
krukle <christofferfareseid@gmail.com>
2022-06-29 13:48:42 +0200

    add voice command for clearing notepad

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from tkinter import N
 from mycroft import MycroftSkill, intent_handler
 from pathlib import Path 
 import os
@@ -6,6 +7,9 @@ DB_DIR = os.path.join(
     str(Path.home()), 'MagicMirror', 'modules', 'MMM-FastNotes', 'backend')
 sys.path.append(DB_DIR)
 from database import Database
+
+YES = ('yes', 'ja')
+NO  = ('no', 'nej')
 
 class Fastnotes(MycroftSkill):
     def __init__(self):
@@ -20,7 +24,7 @@ class Fastnotes(MycroftSkill):
         if note is None:    # Make sure there's a note.
             return self.unspecified_note(message)
         self.speak_dialog("note.taken")
-        self.db.create_post((note,))
+        self.db.create_post(note)
 
     # When the utterance is missing a note.
     # E.g: 'Hey Mycroft, take a note'. 
@@ -32,9 +36,20 @@ class Fastnotes(MycroftSkill):
         note = self.get_response('what.should.i.jot')
         if note is not None: 
             self.speak_dialog('note.taken')
-            self.db.create_post((note,))
+            self.db.create_post(note)
         else:
             self.speak_dialog('something.went.wrong')
+
+    @intent_handler('clear.notepad.intent')
+    def clear_notepad(self, message):
+        is_sure = self.ask_yesno('are.you.sure')
+        if is_sure in YES:
+            self.speak_dialog('notepad.cleared')
+            self.db.delete_all_posts()
+        elif is_sure in NO:
+            self.speak_dialog('notepad.not.cleared')
+        else:
+            self.speak_dialog('could.not.understand')
 
 def create_skill():
     return Fastnotes()
