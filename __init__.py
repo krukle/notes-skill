fd72c03daa3324c42c03dc76b85e779c8018ea57
krukle <christofferfareseid@gmail.com>
2022-06-29 11:20:42 +0200

    notes are now uploaded to db

--- __init__.py
+++ __init__.py
@@ -1,8 +1,16 @@
 from mycroft import MycroftSkill, intent_handler
+from pathlib import Path 
+import os
+import sys
+DB_DIR = os.path.join(
+    str(Path.home()), 'MagicMirror', 'modules', 'MMM-FastNotes', 'backend')
+sys.path.append(DB_DIR)
+from database import Database
 
 class Fastnotes(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.db = Database()
 
     # When the utterance is complete.
     # E.g. 'Hey Mycroft, jot down that I should buy bananas'. (note: 'I should buy bananas')
@@ -12,6 +20,7 @@ class Fastnotes(MycroftSkill):
         if note is None:    # Make sure there's a note.
             return self.unspecified_note(message)
         self.speak_dialog("note.taken")
+        self.db.create_post((note,))
 
     # When the utterance is missing a note.
     # E.g: 'Hey Mycroft, take a note'. 
@@ -23,6 +32,7 @@ class Fastnotes(MycroftSkill):
         note = self.get_response('what.should.i.jot')
         if note is not None: 
             self.speak_dialog('note.taken')
+            self.db.create_post((note,))
         else:
             self.speak_dialog('something.went.wrong')
 
